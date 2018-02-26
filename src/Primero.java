31344714693e4a6fbd6cb8d1ff29fb51a2587946
Adrián Sánchez Díaz <adsandiaz@hotmail.es>
2018-02-26 13:34:02 +0100

    Dos

--- src/Primero.java
+++ src/Primero.java
@@ -2,4 +2,17 @@
 public class Primero {
 	public boolean uno = true;
 	public int tres = 2;
+	/**
+	 * 1. Crea la clase "Primera" 2. Mete un atributo dentro de la clase primera que
+	 * sea entero, haz un commit and push. 3. Crea una segunda clase "Segunda" 4.
+	 * Inserta un atributo dentro de tipo entero, haz commit and push 5. Mete un
+	 * segundo atributo entero en la clase primera y haz commit and push 6. En el
+	 * constructor de la clase segunda, crea un objeto de la clase primeray se
+	 * accede al segundo atributo y se hace commit and push
+	 * 
+	 * Equivocacion: El primer atributo de la clase primera no era entero, era
+	 * boolean. Utiliza la historia para que los cambios hasta el final pero para
+	 * que se queden igual (?)no suar un commit nuevo (No mas de 6)
+	 * 
+	 */
 }
